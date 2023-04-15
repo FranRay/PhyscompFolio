192d1a1e0d2a28a766d17ae4f88e9887fb0446ea
Franchesca Gabrielle Q. Raymundo <franqraymundo@gmail.com>
2023-04-15 21:25:16 +0400

    Updated Infographics Page Functionality
    Added information, text, and links.

--- src/views/ExperimentsView.vue
+++ src/views/ExperimentsView.vue
@@ -11,15 +11,29 @@
 </template>
 
 <style scoped lang = "scss">
-  //site container
-  .site-container {
-    display: block;
-    top: 6em;
-    overflow-y: scroll;
-    width: 90%;
-    height: 85vh;
-    margin-right: auto;
-    margin-left: auto;
-    border: 4px solid var(--clr-secondary); 
-  }
+.site-container {
+  display: block;
+  top: 6em;
+  overflow-y: scroll;
+  width: 90%;
+  height: 85vh;
+  margin-right: auto;
+  margin-left: auto;
+  border: 4px solid var(--clr-secondary); 
+}
+
+.site-container::-webkit-scrollbar {
+    width: 1.2em;
+}
+ 
+.site-container::-webkit-scrollbar-thumb {
+    background: var(--clr-secondary);
+    border-left: 4px solid var(--clr-secondary);
+}
+
+.site-container::-webkit-scrollbar-track {
+    background: var(--clr-primary);
+    border-left: 4px solid var(--clr-secondary);
+}
+
 </style>
