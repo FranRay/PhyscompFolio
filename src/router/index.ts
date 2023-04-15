c45028bd32c3ef54ebbe2e050dd36e7cf2d0b025
Franchesca Gabrielle Q. Raymundo <franqraymundo@gmail.com>
2023-04-15 21:45:47 +0400

    Temporary Main Page
    Changed the main page to infographic temporarily.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue';
+import Infographic from '../views/InfographicView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'infographic',
+      component: Infographic
     },
     {
       path: '/about',
